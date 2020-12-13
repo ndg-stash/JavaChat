fc205405672a2e8d9d9cba2352cb10088e7a7622
Kreat <kreator1989@ya.ru>
2020-12-13 20:10:33 +0300

    Initial commit

---
+++ src/ru/geekbrains/server/ClientHandler.java
@@ -0,0 +1,63 @@
+package ru.geekbrains.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler {
+    private Server server;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    public ClientHandler(Server server, Socket socket) {
+        try {
+            this.server = server;
+            this.socket = socket;
+            this.in = new DataInputStream(socket.getInputStream());
+            this.out = new DataOutputStream(socket.getOutputStream());
+            new Thread(() -> {
+                try {
+                    while (true) {
+                        String str = in.readUTF();
+                        System.out.println("Сообщение от клиента: " + str);
+                        if (str.equals("/end")) {
+                            break;
+                        }
+                        server.broadcastMsg(str);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        out.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    server.unsubscribe(this);
+                }
+            }).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg(String msg) {
+        try {
+            out.writeUTF("ECHO: " + msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/ru/geekbrains/server/Server.java
@@ -0,0 +1,39 @@
+package ru.geekbrains.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+
+public class Server {
+    private Vector<ClientHandler> clients;
+
+    public Server() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(8189);
+            clients = new Vector<>();
+            while (true) {
+                System.out.println("Ждем подключения клиента");
+                Socket socket = serverSocket.accept();
+                ClientHandler c = new ClientHandler(this, socket);
+                subscribe(c);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void subscribe(ClientHandler client) {
+        clients.add(client);
+    }
+
+    public void unsubscribe(ClientHandler client) {
+        clients.remove(client);
+    }
+
+    public void broadcastMsg(String msg) {
+        for (ClientHandler c : clients) {
+            c.sendMsg(msg);
+        }
+    }
+}
